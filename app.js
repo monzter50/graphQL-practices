a0b74c351faeb289414542c858749dabf20af545
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2020-03-03 12:58:42 -0500

    Learning Schemas and Types

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-var schema = buildSchema(`
-  type Query {
-    hello: String,
-    welcome:String
-  }
-`);
-
-const resolvers = {
-    hello: () => 'Hello world!',
-    welcome: () => 'welcome graphQL!', 
-};
-
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers.js')
+var schema = buildSchema(readFileSync(
+  join(__dirname,'lib','schema.graphql'),
+  'utf-8'
+));
+console.log(resolvers)
 const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
