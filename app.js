2295d4378cee44ddd5fd2ee75d54f2321f3c3f57
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2020-03-04 08:40:26 -0500

    Learn arguments elements

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const resolvers = require('./lib/resolvers.js')
-var schema = buildSchema(readFileSync(
+const typeDefs = readFileSync(
   join(__dirname,'lib','schema.graphql'),
   'utf-8'
-));
+)
+var schema = makeExecutableSchema({typeDefs,resolvers});
 console.log(resolvers)
 const app = express();
 app.use('/graphql', graphqlHTTP({
